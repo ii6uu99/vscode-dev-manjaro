d6a2fab5a851ac0f38073a1290aaf321f536e443
“ii6uu99” <ii6uu99@163.com>
2022-11-21 23:05:46 +0800

    git2

---
+++ git3.bat
@@ -0,0 +1,17 @@
+@echo off
+if not exist .git\ (git init)
+if "%1"=="" (if exist .git/config (for /F "tokens=3 delims= " %%A in ('find "url" .git/config') do (if %%A NEQ "" (set remote=%%A) else (set /p remote="Enter Remote URL: "))) else (set /p remote="Enter Remote URL: ")) else (set remote=%1)
+set isgit=%remote:~-4%
+if "%isgit%" NEQ ".git" (set remote=%remote%.git && set url=%remote%) else (set url=%remote:.git=%)
+
+:loop
+git remote add origin %remote%
+git add .
+git commit -m "git2"
+git branch -M main
+git push -u origin main
+
+TIMEOUT 30
+goto loop
+
+start %url%
