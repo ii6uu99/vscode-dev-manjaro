6ed6b4e1943ff52f78d57aa0fb0aa45b2493337b
“ii6uu99” <ii6uu99@163.com>
2022-11-21 23:12:55 +0800

    edit

--- autocommit.bat
+++ autocommit.bat
@@ -25,8 +25,8 @@ git commit  -m "edit"
 git branch -M main
 git push -u origin main
 
-echo 循环了%count%次
-echo restarting-30秒
+echo Completed loop: %count%
+echo restarting
 
 ::确定两次提交之间的时间，目前是每 30 秒一次，您可以更改它！ 
 TIMEOUT 30
