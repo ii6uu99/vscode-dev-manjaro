08029742b6d7d7176820cc1629431ff1c6352ae5
“ii6uu99” <ii6uu99@163.com>
2022-11-21 22:38:30 +0800

    first commit

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# vscode-dev-manjaro" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ii6uu99/vscode-dev-manjaro.git
+git push -u origin main
